cc61170d60a4e12b06d062f895ffbd629ae0c74e
LiliwoL <github@liliwol.fr>
2023-05-10 21:42:05 +0200

    autopush

--- index_next_gen.php
+++ index_next_gen.php
@@ -3,6 +3,7 @@
      * Inclure les fichiers nécessaires
      */
     require_once ('inc/db.php');
+    require_once ('inc/Students.php');
 
     // Instance de DB
     $connecteur = new DB();
@@ -12,6 +13,12 @@
 
     // Appel de la méthode qui va aller chercher la liste de TOUS les étudiants
     $studentsList = $connecteur->findAllStudents();
+
+    // Création d'un objet Etudiant pour l'affichage du tableau des étudiants
+    $studentsObject = new Students();
+
+    // Génération du tableau HTML
+    $output = $studentsObject->displayTable( $studentsList );
 ?>
 <!doctype html>
 <html lang="fr">
@@ -24,35 +31,10 @@
 
 </head>
 <body>
-    <h1>Liste des étudiants</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nom</th>
-                <th scope="col">Prénom</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-                // Pas propre, mais possible. On va placer du code PHP au milieu du HTML
-
-                // Parcours du tableau des étudiants
-                foreach( $studentsList as $student )
-                {
-                    echo '<tr>';
-                        echo '<th scope="row">' . $student['id'] .'</th>';
-                        echo '<td>' . $student['nom'] .'</td>';
-                        echo '<td>' . $student['prenom'] .'</td>';
-                        echo '<td>Editer - Supprimer</td>';
-                    echo '</tr>';
-                }
-            ?>
-        </tbody>
-    </table>
-
-    <a href="edit.php" class="btn btn-primary">Ajout</a>
+    <!--
+        Affichage HTML généré
+    -->
+    <?= $output; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
